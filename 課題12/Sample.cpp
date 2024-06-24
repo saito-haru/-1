5f4659ce3d86fdd518793bf38698893bc0423fa3
saito_haru <230017@jc-21.jp>
2024-06-24 11:19:14 +0900

    課題12追加

---
+++ 課題12/Sample.cpp
@@ -0,0 +1,23 @@
+/*
+	Sample.cpp
+	SampleClassクラスの各メンバ関数を定義
+*/
+//ヘッダをインクルード
+#include <iostream>
+#include "Sample.h"		//クラスを宣言しているヘッダ
+
+//メンバ変数に値を代入するメンバ関数
+void SampleClass::Input() {
+	a = 10;
+	b = 3;
+}
+
+//メンバ変数同士の計算を行うメンバ関数
+void SampleClass::Plus() {
+	c = a + b;
+}
+
+//メンバ変数同士の計算を行うメンバ関数
+void SampleClass::Disp() {
+	std::cout << "変数Cの値は" << c << "\n";
+}
---
+++ 課題12/main.cpp
@@ -0,0 +1,19 @@
+#include "Sample.h"
+
+//SampleClass クラスのポインタを用意
+SampleClass* a;
+
+//メイン関数
+int main()
+{
+	//クラスのインスタンス（実態）を作る
+	a = new SampleClass();
+
+	//３つのメンバ関数を呼び出す
+	a->Input();
+	a->Plus();
+	a->Disp();
+
+	//使い終わったインスタンスを削除
+	delete a;
+}
